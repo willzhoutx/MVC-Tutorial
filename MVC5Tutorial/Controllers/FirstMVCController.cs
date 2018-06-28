c9bc55cbaf254dccb2248fcbce41663631572b18
Will Zhou <34556408+willzhoutx@users.noreply.github.com>
2018-06-28 14:15:20 -0500

    Add project via upload.

---
+++ MVC5Tutorial/Controllers/FirstMVCController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Tutorial.Controllers
+{
+    public class FirstMVCController : Controller
+    {
+        // GET: FirstMVC
+        public ActionResult SayHello()
+        {
+            return View("HelloView");
+        }
+    }
+}
