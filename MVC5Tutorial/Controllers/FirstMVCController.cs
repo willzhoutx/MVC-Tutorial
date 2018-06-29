2e153cf0dc735c3d5e0fc92b3ad8b333341bcc2d
Will Zhou <willzhoutx@gmail.com>
2018-06-29 15:51:15 -0500

    Use ViewBag to pass data from Controller to View

--- MVC5Tutorial/Controllers/FirstMVCController.cs
+++ MVC5Tutorial/Controllers/FirstMVCController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC5Tutorial.Models;
 
 namespace MVC5Tutorial.Controllers
 {
@@ -12,6 +13,14 @@ namespace MVC5Tutorial.Controllers
         public ActionResult SayHello()
         {
             ViewData["CurrentTime"] = DateTime.Now.ToString();
+
+            Employee emp = new Employee();
+            emp.FirstName = "Joe";
+            emp.LastName = "Blow";
+            emp.Salary = 80000;
+            //ViewData["Employee"] = emp;
+            ViewBag.Employee = emp;
+
             return View("HelloView");
         }
     }
